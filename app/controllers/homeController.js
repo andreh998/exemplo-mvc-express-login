08be76eda01ec74f1e20883aa184334e856d9031
André Hoffmann <andreh998@gmail.com>
2018-12-04 23:03:18 -0200

    Ajustes na sessao

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -9,7 +9,7 @@ var homeModel = require('../models/homeModel');
 
 //exportamos a funcao responsavel por essa requisicao
 module.exports.index = function(request, response){
-    //apenas retorna a home
-    response.render('home/home');
+    //apenas retorna a home com a sessao do usuario
+    response.render('home/home', {sessao:request.session.user});
 
 };
